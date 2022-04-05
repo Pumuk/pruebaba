d085b403a9d745f1bd2d160186eead375ebdd19b
pumuk <pumukle@hotmail.com>
2022-04-05 19:40:26 +0200

    prueba commit

---
+++ PRACTICA4/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRACTICA4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tcBanco.Visibility = Visibility.Visible;
+            tcFormulario.Visibility = Visibility.Hidden;
+        }
+
+        private void mBanco_Click(object sender, RoutedEventArgs e)
+        {
+            tcBanco.Visibility = Visibility.Visible;
+            tcFormulario.Visibility = Visibility.Hidden;
+        }
+
+        private void mFormulario_Click(object sender, RoutedEventArgs e)
+        {
+            tcBanco.Visibility = Visibility.Hidden;
+            tcFormulario.Visibility = Visibility.Visible;
+        }
+    }
+}
